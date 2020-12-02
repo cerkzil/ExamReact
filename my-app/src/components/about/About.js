221e1443d8e1d5dc143d0dcd911f6c8cdae5ed22
cerkzil <39460996+cerkzil@users.noreply.github.com>
2020-12-02 16:53:28 +0200

    final update

--- my-app/src/components/about/About.js
+++ my-app/src/components/about/About.js
@@ -9,6 +9,7 @@ class About extends Component {
         <h2>About Me:</h2>
         <p>I'm Žilvinas Čerkauskas a student at <a href="http://kitm.lt/" target="_blank" rel="noreferrer">KITM</a></p>
         <p>You can find everything I code at: <a href="https://github.com/cerkzil/" target="_blank" rel="noreferrer">GitHub</a> and everything I create at: <a href="https://www.artstation.com/milkysenpai/" target="_blank" rel="noreferrer">ArtStation</a>😋</p>
+        <p>This Exam was suprisingly straight forward but also challenging, I mostly got stuck when I named variables one thing but remembered it as another😅</p>
       </Container>
     )
   }
